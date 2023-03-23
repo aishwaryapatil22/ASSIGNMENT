d644e2f22a9ba87f1c9fb024b6a82a42440dfa5f
aishwaryapatil22 <127470379+aishwaryapatil22@users.noreply.github.com>
2023-03-23 13:18:11 +0530

    Add files via upload

---
+++ postivenegtive.java
@@ -0,0 +1,22 @@
+class postivenegtive{
+    static void Number(int num){
+      
+        if(num>0)
+            System.out.println(num + " is POSITIVE NUMBER.");
+        else 
+            System.out.println(num + " is NEGATIVE NUMBER.");
+       
+    }
+    public static void main( String args[] ) {
+       
+        int no1 = 20;
+        int no2 = -100;
+        
+
+       
+        Number(no1);
+Number(no2);
+
+        
+    }
+}
