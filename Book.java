0a11127e6b9c75233dea62edd85e0d6f8b37b946
aishwaryapatil22 <127470379+aishwaryapatil22@users.noreply.github.com>
2023-04-26 09:51:34 +0530

    Add files via upload

--- Book.java
+++ Book.java
@@ -1,35 +1,43 @@
-package perform;
+package BookSystem;
 
-
-class Book {
-    String name;
-    int price;
-    Author author;
-    Publisher publisher;
-
-    public Book(String name, int price, Author author, Publisher publisher) {
-        this.name = name;
-        this.price = price;
-        this.author = author;
-        this.publisher = publisher;
-    }
-
-    public void display() {
-        System.out.println("Book name: " + name);
-        System.out.println("Book price: " + price);
-        System.out.println("---------------------Author Details------------------------");
-        System.out.println("Author Name: " + author.name);
-        System.out.println("Author Age: " + author.age);
-        System.out.println("Author Place: " + author.place);
-        System.out.println("---------------------Publisher Details------------------------");
-        System.out.println("Publisher Name: " + publisher.name);
-        System.out.println("Publisher ID: " + publisher.id);
-        System.out.println("Publisher City: " + publisher.city);
-    }
-    public static void main(String[] args) {
-        Author author = new Author("Micheal", 32, "USA");
-        Publisher publisher = new Publisher("Sun Publisher", "ABC-1110", "London");
-        Book book = new Book("Core Java", 800, author, publisher);
-        book.display();
-    }
+public class Book {
+	
+	private int bookId;
+	private String bookName;
+	private double bookPrice;
+	
+	//getter setter method 
+	public int getBookId() {
+		return bookId;
+	}
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public double getBookPrice() {
+		return bookPrice;
+	}
+	public void setBookPrice(double bookPrice) {
+		this.bookPrice = bookPrice;
+	}
+	
+	//no arg cosntructor
+	
+	public Book() {
+		super();
+		
+	}
+	
+	//parameterized constructor
+	public Book(int bookId, String bookName, double bookPrice) {
+		super();
+		this.bookId=bookId;
+		this.bookName=bookName;
+		this.bookPrice=bookPrice;
+	}
 }
